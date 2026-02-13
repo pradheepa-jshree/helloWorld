32069338b29785689f5084919bae42c410896fd7
pradheepa jaya shree <pradheepajayashree@gmail.com>
2026-02-13 15:33:02 +0530

    Subtraction code

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,10 @@
-public class helloWorld{
-    public static void main(String[] args){
-        System.out.println("helloworld");
+public class helloWorld {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        int sub = a - b;
+
+        System.out.println("subtraction = " + sub);
     }
 }
